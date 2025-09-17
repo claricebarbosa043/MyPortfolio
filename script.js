35675420ecdb642cc7690c615853902bd8affc90
ClariceBarbosa43 <23-34981@g.batstate-u.edu.ph>
2025-09-17 11:03:19 +0800

    portfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+const myName = "Clarice Barbosa";
+
+// Greeting update
+function updateGreeting() {
+  const hour = new Date().getHours();
+  let greeting = "Hi, I'm " + myName;
+  if (hour < 12) greeting = "Good Morning! I'm " + myName;
+  else if (hour < 18) greeting = "Good Afternoon! I'm " + myName;
+  else greeting = "Hi! I'm " + myName;
+  document.getElementById('greeting').innerText = greeting;
+}
+
+// Alerts
+function showMessage() {
+  alert("Thank you for visiting my portfolio! Email me at: 23-34981@g.batstate-u.edu.ph");
+}
+function showSocial(platform) {
+  alert("Follow me on " + platform + " (Clarice Barbosa)" + " !");
+}
+function viewPhoto(img) {
+  alert("Viewing: " + img.alt);
+}
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// On load
+window.onload = updateGreeting;
